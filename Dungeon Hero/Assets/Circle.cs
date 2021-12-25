a1f71f2230d30bdd31a919613b1b18b39c5765f2
templarassVN <53231722+templarassVN@users.noreply.github.com>
2021-12-25 22:51:46 +0700

    First Test

---
+++ Dungeon Hero/Assets/Circle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(100 * Time.deltaTime, 0);
+    }
+}
