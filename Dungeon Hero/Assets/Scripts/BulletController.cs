d22fb5a1c7113bd12e80850b4b569eefdc2d920c
templarassVN <53231722+templarassVN@users.noreply.github.com>
2022-01-04 10:14:40 +0700

    update

---
+++ Dungeon Hero/Assets/Scripts/BulletController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed = 7.5f;
+    private Rigidbody2D rigidBody;
+    float timeDestroy = 2.0f;
+    float timeCount = 0.0f;
+    public GameObject impactEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.velocity = transform.right * speed;
+        timeCount += Time.deltaTime;
+        if (timeCount >= timeDestroy)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
