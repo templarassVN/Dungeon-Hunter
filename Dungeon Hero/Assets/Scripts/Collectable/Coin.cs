030f83a41821cd241ee78fc033b97147557c6c4e
templarassVN <53231722+templarassVN@users.noreply.github.com>
2022-01-12 16:50:10 +0700

    Rearrange player

--- Dungeon Hero/Assets/Scripts/Collectable/Coin.cs
+++ Dungeon Hero/Assets/Scripts/Collectable/Coin.cs
@@ -12,7 +12,7 @@ public class Coin : MonoBehaviour
     {
         if(collision != null && collision.name.Equals("Player"))
         {
-            PlayerController.instance.ChangeMoney(_amount);
+            
             Destroy(gameObject);
         }
 
